a7bf177ba402fee862c919e1dec9e071628cc2c9
Harmanjot Singh <152049009+HarmanSandhu-glitch@users.noreply.github.com>
2026-03-03 12:28:26 +0530

    Update CORS origin for production deployment

--- server/index.js
+++ server/index.js
@@ -24,7 +24,7 @@ app.post(
 );
 
 app.use(cors(
-    {origin:"http://localhost:5173",
+    {origin:"https://exam-notes-frontend.vercel.app/",
         credentials:true,
         methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
     }
@@ -51,4 +51,5 @@ app.use("/api/folder", folderRouter)
 app.listen(PORT,()=>{
     console.log(`✅ Server running on port ${PORT}`)
     connectDb()
+
 })
